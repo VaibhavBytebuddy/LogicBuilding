f7ecc9f0cc0529742f38a55cade2dfe511c4f9d5
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-06-26 13:25:20 +0530

    June 26 2025 from today tree start

---
+++ src/Trees/CreateTree.java
@@ -0,0 +1,35 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class CreateTree {
+
+    public static void main(String[] args) {
+        TreeNode root=new TreeNode(1);
+        root.left=null;
+        root.right=new TreeNode(2);
+        root.right.left=new TreeNode(3);
+
+        System.out.println("Preorder Traversal");
+        List<Integer> order = preorderTraversal(root);
+        System.out.println(order);
+
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> li = new ArrayList<>();
+        preorderHelper(root, li);
+        return li;
+
+    }
+    public static void preorderHelper(TreeNode root, List<Integer> li) {
+        if (root == null) return;
+
+        li.add(root.val);               // visit root
+        preorderHelper(root.left, li);  // visit left
+        preorderHelper(root.right, li); // visit right
+    }
+}
---
+++ src/Trees/TreeNode.java
@@ -0,0 +1,14 @@
+package Trees;
+
+import com.sun.source.tree.Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
