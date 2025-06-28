4d1148184562dbb81eab728bf7825a57823318bf
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-06-28 12:37:39 +0530

    28-06-25

---
+++ src/Trees/BinarySearchTree.java
@@ -0,0 +1,20 @@
+package Trees;
+
+public class BinarySearchTree {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if(root.val<val)
+        {
+            return searchBST(root.right,val);
+        }else
+        {
+            return    searchBST(root.left,val);
+        }
+
+
+
+    }
+}
